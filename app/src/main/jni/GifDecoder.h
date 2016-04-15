ec347099b0d1a7eac7e97e194587e2f92aa9b572
withparadox2 <withparadox2@gmail.com>
2016-04-15 17:51:28 +0800

    Indent.

--- app/src/main/jni/GifDecoder.h
+++ app/src/main/jni/GifDecoder.h
@@ -16,91 +16,91 @@ template <typename T> void log(const char *name, T value);
 bool skipBlock(ifstream& is);
 class ByteEater {
 public:
-	virtual bool eat(std::ifstream& is);
-	virtual ~ByteEater();
-	bool skip(std::ifstream& is);
-	bool readu1(ifstream& is, u1 *dest);
-	short convertu2(char* bytes);
+  virtual bool eat(std::ifstream& is);
+  virtual ~ByteEater();
+  bool skip(std::ifstream& is);
+  bool readu1(ifstream& is, u1 *dest);
+  short convertu2(char* bytes);
 };
 
 class LSD : public ByteEater {
 public:
-	void resolvePacketFields(char& fileds);
-	bool eat(std::ifstream& is);
-	u2 sWidth;
-	u2 sHeight;
-	u1 bgIndex;
-	bool hasGTable;
-	u1 sizeGTable;
+  void resolvePacketFields(char& fileds);
+  bool eat(std::ifstream& is);
+  u2 sWidth;
+  u2 sHeight;
+  u1 bgIndex;
+  bool hasGTable;
+  u1 sizeGTable;
 };
 
 class ColorTable : ByteEater {
 public:
-	int tableSize;
-	ColorTable(int size);
-	bool eat(ifstream& is);
-	char *colors;
-	u1 red(int index);
-	u1 green(int index) ;
-	u1 blue(int index) ;
-	~ColorTable();
+  int tableSize;
+  ColorTable(int size);
+  bool eat(ifstream& is);
+  char *colors;
+  u1 red(int index);
+  u1 green(int index) ;
+  u1 blue(int index) ;
+  ~ColorTable();
 };
 
 class AppExtBlock : ByteEater {
 public:
-	bool eat(ifstream& is);
+  bool eat(ifstream& is);
 };
 
 class GraphicCtrlExt : ByteEater {
 public:
-	bool eat(ifstream& is) ;
-	u1 delay;
-	bool transparency;
-	u1 transparencyIndex;
+  bool eat(ifstream& is) ;
+  u1 delay;
+  bool transparency;
+  u1 transparencyIndex;
 };
 
 class ImageDes : ByteEater {
 public:
-	bool eat(ifstream& is);
-	u2 leftPos;
-	u2 topPos;
-	u2 iWidth;
-	u2 iHeight;
-	bool hasLTable;
-	bool interlace;
-	u1 sizeLTable;
+  bool eat(ifstream& is);
+  u2 leftPos;
+  u2 topPos;
+  u2 iWidth;
+  u2 iHeight;
+  bool hasLTable;
+  bool interlace;
+  u1 sizeLTable;
 };
 struct Dict {
-	char value;
-	int len;
-	int preIndex;
+  char value;
+  int len;
+  int preIndex;
 };
 
 class LZWDecoder : ByteEater {
 public:
-	LZWDecoder(LSD *lsd_p, ImageDes* imgDes_p);
-	u1 *pixels;
-	bool eat(ifstream& is);
+  LZWDecoder(LSD *lsd_p, ImageDes* imgDes_p);
+  u1 *pixels;
+  bool eat(ifstream& is);
 private:
-	LSD *lsd;
-	ImageDes *imgDes;
+  LSD *lsd;
+  ImageDes *imgDes;
 };
 
 class GifDecoder {
 public:
-	GifDecoder();
-	~GifDecoder();
-	void loadGif(const char *file);
-	void processStream(ifstream& is);
+  GifDecoder();
+  ~GifDecoder();
+  void loadGif(const char *file);
+  void processStream(ifstream& is);
 
-	u1* pixels;
-	int width;
-	int height;
+  u1* pixels;
+  int width;
+  int height;
 
-	u4* getPixels() ;
-	ColorTable* getColorTable();
-	ColorTable *gct;
-	ColorTable *fuck;
+  u4* getPixels() ;
+  ColorTable* getColorTable();
+  ColorTable *gct;
+  ColorTable *fuck;
 
 private:
 };
@@ -108,29 +108,29 @@ private:
 template<typename _Tp>
 class array_ptr {
 private:
-	_Tp *m_pointer;
+  _Tp *m_pointer;
 public:
-	typedef _Tp element_type;
-	explicit array_ptr(element_type* p = 0): m_pointer(p) {}
-	~array_ptr() {
-		delete m_pointer;
-	}
-	element_type& operator*() const {
-		assert(m_pointer != 0);
-		return *m_pointer;
-	}
-	element_type* operator->() const {
-		assert(m_pointer != 0);
-		return m_pointer;
-	}
-	element_type* get() const {
-		return m_pointer;
-	}
-	element_type* release() {
-		element_type* tmp = m_pointer;
-		m_pointer = 0;
-		return tmp;
-	}
+  typedef _Tp element_type;
+  explicit array_ptr(element_type* p = 0): m_pointer(p) {}
+  ~array_ptr() {
+    delete m_pointer;
+  }
+  element_type& operator*() const {
+    assert(m_pointer != 0);
+    return *m_pointer;
+  }
+  element_type* operator->() const {
+    assert(m_pointer != 0);
+    return m_pointer;
+  }
+  element_type* get() const {
+    return m_pointer;
+  }
+  element_type* release() {
+    element_type* tmp = m_pointer;
+    m_pointer = 0;
+    return tmp;
+  }
 
 };
 void exportGifToTxt(GifDecoder* result);
